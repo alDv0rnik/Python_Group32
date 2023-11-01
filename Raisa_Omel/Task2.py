b5504d04c0a9f8523a0387fb0d0bdc32aa94d4b8
OmelRaisa <omelraisa@mail.ru>
2023-11-01 22:26:04 +0300

    all homework

---
+++ Raisa_Omel/Task2.py
@@ -0,0 +1,4 @@
+#Task 2
+age = int(input('Введите возраст '))
+name = input('Введите имя ')
+print('My name is '+name+' and I am '+str(age))
---
+++ Raisa_Omel/Task7.py
@@ -0,0 +1,4 @@
+str1=input('введите строку ').lower()
+str2=input('введите строку ').lower()
+print('первый индекс вхождения '+str(str1.find(str2))) #15
+print('последний индекс вхождения '+str(str1.rfind(str2))) #31
