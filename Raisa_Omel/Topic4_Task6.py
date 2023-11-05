497c5cb59a1b8e7867eb9c0f635449d20812d5c0
OmelRaisa <omelraisa@mail.ru>
2023-11-05 20:30:48 +0300

    homework 2

--- Raisa_Omel/Topic4_Task6.py
+++ Raisa_Omel/Topic4_Task6.py
@@ -1,3 +1,3 @@
 x1, y1 = map(int,input('Введите 2 координаты 1 точки через пробел ').split())
 x2, y2 = map(int,input('Введите 2 координаты 2 точки через пробел ').split())
-print(1 if x1==x2 or y1==y2 or x1-x2==y1-y2 else 0)
+print(1 if x1==x2 or y1==y2 or abs(x1-x2)==abs(y1-y2) else 0)
