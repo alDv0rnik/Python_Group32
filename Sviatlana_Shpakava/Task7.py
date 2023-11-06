6b9e8645d59081a518f5b5064b3a135f2186685c
Shpakava <cranberries66666@mail.ru>
2023-11-06 12:48:00 +0300

    created Task7.py

---
+++ Sviatlana_Shpakava/Task7.py
@@ -0,0 +1,7 @@
+x1 = int(input())
+x2 = int(input())
+y1 = int(input())
+y2 = int(input())
+if (abs(x1 - x2) == 1 and abs(y1 - y2) == 2) or (abs(x1 - x2) == 2 and abs(y1 - y2) == 1):
+    print("good")
+else: print("not good")
