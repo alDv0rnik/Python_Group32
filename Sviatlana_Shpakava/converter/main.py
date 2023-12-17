9ff66b0e4bf3680441635f457e4b78d43c8108e4
Shpakava <cranberries66666@mail.ru>
2023-12-17 21:45:16 +0300

    created main.py

---
+++ Sviatlana_Shpakava/converter/main.py
@@ -0,0 +1,22 @@
+from converter_a import converter_usd
+from converter_b import converter_eur
+from converter_c import converter_rub
+
+
+def convert_this(sum_byn, currency):
+    if currency == 'usd':
+        return converter_usd(sum_byn)
+    elif currency == 'eur':
+        return converter_eur(sum_byn)
+    return converter_rub(sum_byn)
+
+
+# if __name__ == '__main__':
+#     import sys
+#     sum_byn = sys.argv[1]
+#     currency = sys.argv[-1]
+#     print(convert_this)
+
+print(convert_this(sum_byn=100, currency='usd'))
+print(convert_this(sum_byn=100, currency='eur'))
+print(convert_this(sum_byn=100, currency='rub'))
